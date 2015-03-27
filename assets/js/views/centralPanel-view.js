111a31d07d0b3ea96bb407837d755998c7b48a29
Johan Pascal <johan.pascal@belledonne-communications.com>
2015-03-27 23:40:29 +0100

    Use d3 to draw countries highlight on rfmp map

--- assets/js/views/centralPanel-view.js
+++ assets/js/views/centralPanel-view.js
@@ -33,27 +33,66 @@ sharksDB.Views.CentralPanel = Backbone.View.extend({
 				/* render rfmo map */
 				this.$el.append("<div id='map'></div>");
 				this.$el.show(); /* display the map div before loading the map to get correct dimension */
+				/* load map from mapbox */
 				L.mapbox.accessToken = 'pk.eyJ1IjoiamVhbm5vdGxhcGluIiwiYSI6Im5qNTl1QXcifQ.fex2-4xMOYtkSgwtkwRGBQ';
 				var map = L.mapbox.map('map', 'jeannotlapin.lcld15nl', {worldCopyJump: true}).setView(sharksDB.Collections.RFMOInfoList[rfmo].map, 2);
+
+				/* add countries highlighting layer using d3 */
 				var ue = ($.inArray(rfmo, sharksDB.Collections.countryInfoList[1001].rfmo) != -1); /* UE have the arbitrary 1001 iso code in the DB */
-				var featureLayer = L.mapbox.featureLayer()
-				    .loadURL('data/geodata/countries110.json')
-				    .setFilter(function (feature,layer) {
-					    /* check country */
-					    if (+feature.properties.iso_n3 in sharksDB.Collections.countryInfoList && sharksDB.Collections.countryInfoList[+feature.properties.iso_n3] != undefined) {
-						    if ($.inArray(rfmo, sharksDB.Collections.countryInfoList[+feature.properties.iso_n3].rfmo) != -1) {
-							    return true
-						    }
-					    }
-					    /* check UE */
-					    if (ue==true && feature.properties.UE==1) { /* geojson has been modified to include a UE property set to 1 for UE members */
-						    return true;
-					    }
-					    return false;
-				    })
-				    .addTo(map);
-				featureLayer.on('ready', function() {
-					featureLayer.setStyle({opacity: 0.4, fillOpacity:0.25, color:'#3dd5a8', fillColor:'#3dd5a8'});
+				var svg = d3.select(map.getPanes().overlayPane).append("svg"),
+				g = svg.append("g").attr("class", "leaflet-zoom-hide");
+
+				d3.json("data/geodata/countries110.json", function(collection) {
+					var transform = d3.geo.transform({point: projectPoint}),
+					path = d3.geo.path().projection(transform);
+
+					var feature = g.selectAll("path")
+						.data(collection.features)
+						.enter()
+						.append("path")
+						.filter(function (d){
+							/* check country */
+							if (+d.properties.iso_n3 in sharksDB.Collections.countryInfoList && sharksDB.Collections.countryInfoList[+d.properties.iso_n3] != undefined) {
+								if ($.inArray(rfmo, sharksDB.Collections.countryInfoList[+d.properties.iso_n3].rfmo) != -1) {
+									return true
+								}
+							}
+
+							/* check UE */
+							if (ue==true && d.properties.UE==1) { /* geojson has been modified to include a UE property set to 1 for UE members */
+								return true;
+							}
+							return false;
+						})
+						.style("fill", "#3dd5a8")
+						.style("fill-opacity", 0.25)
+						.style("stroke", "#3dd5a8")
+						.style("stroke-opacity", 0.5);
+
+					map.on("viewreset", reset);
+					reset();
+
+					// Reposition the SVG to cover the features.
+					function reset() {
+						var bounds = path.bounds(collection),
+						topLeft = bounds[0],
+						bottomRight = bounds[1];
+
+						svg.attr("width", bottomRight[0] - topLeft[0])
+							.attr("height", bottomRight[1] - topLeft[1])
+							.style("left", topLeft[0] + "px")
+							.style("top", topLeft[1] + "px");
+
+						g.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
+
+						feature.attr("d", path);
+					}
+
+					// Use Leaflet to implement a D3 geometric transformation.
+					function projectPoint(x, y) {
+						var point = map.latLngToLayerPoint(new L.LatLng(y, x));
+						this.stream.point(point.x, point.y);
+					}
 				});
 
 				/* add the 200nm limit from FAO server */
