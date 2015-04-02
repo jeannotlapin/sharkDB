08dc25b9eb9d81d34a1c79dba92ae60bbb3fbe6b
Johan Pascal <johan.pascal@belledonne-communications.com>
2015-04-02 23:52:12 +0200

    Replace spaces by _ in URL parameters

--- assets/js/routers/app-routers.js
+++ assets/js/routers/app-routers.js
@@ -1,42 +1,41 @@
 sharksDB.Routers.App = Backbone.Router.extend({
 
-  routes: {
-	"": "reset",
-	"country/:id": "country",
-	"rfmo/:id": "rfmo",
-	"species/:id": "species"
-  },
+	routes: {
+		"": "reset",
+		"country/:id": "country",
+		"rfmo/:id": "rfmo",
+		"species/:id": "species"
+	},
 
-  initialize: function() {
-    sharksDB.Models.currentState.on("change", this.updateNav, this);
-  },
+	initialize: function() {
+		sharksDB.Models.currentState.on("change", this.updateNav, this);
+	},
 
-  reset : function(){
-	sharksDB.Models.currentState.set({country: '', rfmo: '', species: ''});
-  },
+	reset : function(){
+		sharksDB.Models.currentState.set({country: '', rfmo: '', species: ''});
+	},
 
-  country: function(id) {
-	sharksDB.Models.currentState.set({country: id, rfmo: '', species: ''});
-  },
+	country: function(id) {
+		sharksDB.Models.currentState.set({country: id, rfmo: '', species: ''});
+	},
 
-  rfmo: function(id) {
-	sharksDB.Models.currentState.set({country: '', rfmo: id, species: ''});
-  },
+	rfmo: function(id) {
+		sharksDB.Models.currentState.set({country: '', rfmo: id, species: ''});
+	},
 
-  species: function(id) {
-	sharksDB.Models.currentState.set({country: '', rfmo: '', species: id});
-  },
+	species: function(id) {
+		sharksDB.Models.currentState.set({country: '', rfmo: '', species: id.replace(/_/g, ' ')});
+	},
 
-  updateNav: function() {
-	  if (sharksDB.Models.currentState.get('country') != '') {
-	  	  this.navigate('country/'+sharksDB.Models.currentState.get('country'), {trigger: false});
-	  } else if (sharksDB.Models.currentState.get('rfmo') != '') {
-	  	  this.navigate('rfmo/'+sharksDB.Models.currentState.get('rfmo'), {trigger: false});
-	  } else if (sharksDB.Models.currentState.get('species') != '') {
-	  	  this.navigate('species/'+sharksDB.Models.currentState.get('species'), {trigger: false});
-	  } else {
-		  this.navigate('', {trigger: false});
-	  }
-  }
-
+	updateNav: function() {
+		if (sharksDB.Models.currentState.get('country') != '') {
+			this.navigate('country/'+sharksDB.Models.currentState.get('country'), {trigger: false});
+		} else if (sharksDB.Models.currentState.get('rfmo') != '') {
+			this.navigate('rfmo/'+sharksDB.Models.currentState.get('rfmo'), {trigger: false});
+		} else if (sharksDB.Models.currentState.get('species') != '') {
+			this.navigate('species/'+sharksDB.Models.currentState.get('species').replace(/ /g, '_'), {trigger: false});
+		} else {
+			this.navigate('', {trigger: false});
+		}
+	}
 });
